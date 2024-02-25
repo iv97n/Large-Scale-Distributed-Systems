16bd74f75104903f63f916d576d033d23fdc718b
martioms01 <115492151+martioms01@users.noreply.github.com>
2024-02-25 14:06:20 +0100

    Add files via upload

---
+++ lab2/src/test/java/edu/upf/TwitterFilterTest.java
@@ -0,0 +1,26 @@
+package edu.upf;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+//import edu.upf.filter.FileLanguageFilterApp;
+//import edu.upf.model.SimplifiedTweet;
+
+/**
+ * Unit test for simple App.
+ */
+public class TwitterFilterTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+
+
+    
+}
