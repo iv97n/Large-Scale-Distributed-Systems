b7aa7e666d0ec95940e74c08eca16564b6f826ed
iv97n <ivan.hernandez04@estudiant.upf.edu>
2024-03-02 04:30:52 +0100

    debugging ex4

--- lab2/src/main/java/spark/MostRetweetedApp.java
+++ lab2/src/main/java/spark/MostRetweetedApp.java
@@ -59,61 +59,55 @@ public class MostRetweetedApp {
         for (Tuple2<Integer, Long> tuple : top10_user) {
             top10UserIds.add(tuple._2());
         }  
-          
-        JavaRDD<Tuple2<Long, Long>> top_tweetid_userid = sparkContext.emptyRDD();
+
+
+        JavaRDD<Tuple2<Long, Tuple2<Long, String>>> emptyRDD = sparkContext.emptyRDD();
+        JavaPairRDD<Long, Tuple2<Long, String>> top_tweetid_userid= JavaPairRDD.fromJavaRDD(emptyRDD);
+        top_tweetid_userid.persist(StorageLevel.MEMORY_ONLY());
 
         // For each user compute its tweets, count, sort them, and select first one
         for (Long userId : top10UserIds) {
             // Filter the RDD so it only contains retweeted tweets referring to the given user
-            JavaRDD<Optional<ExtendedSimplifiedTweet>> user_retweeted_tweets = filtered_tweets.filter(tweet -> tweet.get().getRetweetedUserId() == userId);
-            // Convert the RDD from single-valued to key-value of the form (retweeted id, 1)
-            JavaPairRDD<Long, Integer> retweeted_id_pair = user_retweeted_tweets.mapToPair(tweet -> {
+            JavaRDD<Optional<ExtendedSimplifiedTweet>> user_retweeted_tweets = filtered_tweets.filter(tweet -> tweet.get().getRetweetedUserId().compareTo(userId) == 0);
+
+            // Convert the RDD from single-valued Optional<ExtendedSimplifiedTweet> instances to key-value of the form (key: (retweeted id, retweeted text), value: 1)
+            JavaPairRDD<Tuple2<Long, String>, Integer> retweeted_id_pair = user_retweeted_tweets.mapToPair(tweet -> {
                 long retweeted_id = tweet.get().getRetweetedId();
-                return new Tuple2<>(retweeted_id , 1);
+                String tweet_text = tweet.get().getText();
+                Tuple2<Long, String> key = new Tuple2<>(retweeted_id, tweet_text);
+                return new Tuple2<>(key, 1);
             });
 
-            // Reduce by key to count the number of times each tweet (retweeted id) has been retweeted
-            JavaPairRDD<Long, Integer> retweeted_id_count = retweeted_id_pair.reduceByKey((a, b) -> a + b);      
+            
+            // Reduce by key to count the number of times each tweet has been retweeted
+            JavaPairRDD<Tuple2<Long, String>, Integer> retweeted_id_count = retweeted_id_pair.reduceByKey((a, b) -> a + b); 
+        
 
-            // Swap key and value to obtain a tuple of the form (retweeted id count, retweeted id)
-            JavaPairRDD<Integer, Long> retweeted_id_count_swapped = retweeted_id_count.mapToPair(tuple -> new Tuple2<>(tuple._2(), tuple._1()));
+            // Swap key and value to obtain a tuple of the form (key: retweeted count, value: (retweeted id, retweeted text))
+            JavaPairRDD<Integer, Tuple2<Long, String>> retweeted_id_count_swapped = retweeted_id_count.mapToPair(tuple -> new Tuple2<>(tuple._2(), tuple._1()));
 
             // Sort by key (retweeted id count) in descending order
-            JavaPairRDD<Integer, Long> sorted_retweeted_id_count_swapped = retweeted_id_count_swapped.sortByKey(false);
+            JavaPairRDD<Integer, Tuple2<Long, String>> sorted_retweeted_id_count_swapped = retweeted_id_count_swapped.sortByKey(false);
 
             // Take the most retweeted tweed
-            List<Tuple2<Integer, Long>> most_retweeted_id_count_list = sorted_retweeted_id_count_swapped.take(1);
+            List<Tuple2<Integer, Tuple2<Long, String>>> most_retweeted_id_count_list = sorted_retweeted_id_count_swapped.take(1);
 
-            // Select the retweeted tweet id
-            List<Long> most_retweeted_id_list = new ArrayList<>();
-            most_retweeted_id_list.add(most_retweeted_id_count_list.get(0)._2);
-
-            // Convert the retweeted tweet id into an RDD
-            JavaRDD<Long> most_retweeted_id_swapped = sparkContext.parallelize(most_retweeted_id_list);       
+            JavaRDD<Tuple2<Integer, Tuple2<Long, String>>> most_retweeted_id_swapped = sparkContext.parallelize(most_retweeted_id_count_list);    
             
-            // Map the retweeted tweet id RDD into an RDD conaining a tuple (tweetid, userid) (It is a single-valued RDD, not a key-value RDD)
-            JavaRDD<Tuple2<Long, Long>> most_retweeted_id = most_retweeted_id_swapped.map(retweeted_id -> new Tuple2<>(retweeted_id, userId));
+            // Map the retweeted tweet id RDD into an RDD conaining a tuple (userId, (tweetId, tweetText)) (It is a single-valued RDD, not a key-value RDD)
+            JavaPairRDD<Long, Tuple2<Long, String>> most_retweeted_id = most_retweeted_id_swapped.mapToPair(retweeted -> new Tuple2<>(userId, retweeted._2));
                       
             // Append the newly created Tuple to the RDD containing the tweetid userid pairs
-            top_tweetid_userid  = top_tweetid_userid .union(most_retweeted_id);
+            top_tweetid_userid  = top_tweetid_userid.union(most_retweeted_id);
 
         }
         
-  
-        // RDD with key = tweetid, value = userid
-        JavaPairRDD<Long,Long> paired_top10_retweets = top_tweetid_userid.mapToPair(pair -> new Tuple2<>(pair._1(), pair._2()));
-        
-        // Select tweetid of top 10 most retweeted users as a list
-        List<Long> tweet_top10 = paired_top10_retweets.map(value -> value._1()).collect();
 
-        // Rdd (tweetid, text) 
-        JavaPairRDD<Long, String> top10_id_to_text = top10_user_retweeted.filter(tweet -> tweet_top10.contains(tweet.get().getRetweetedUserId())).mapToPair(tweet -> new Tuple2<>(tweet.get().getTweetId(), tweet.get().getText()));
         
-        // Save RDD in one single file
-        top10_id_to_text = top10_id_to_text.coalesce(1);
+        top_tweetid_userid = top_tweetid_userid.coalesce(1);
 
-        top10_id_to_text.saveAsTextFile(outputPath);
-        
+        top_tweetid_userid.saveAsTextFile(outputPath);
+
         // Stop Spark context
         sparkContext.stop();
     }
