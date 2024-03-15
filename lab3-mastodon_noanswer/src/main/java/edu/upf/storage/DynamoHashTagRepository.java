e5538e789b104d051f2bb70bb25a99ffff1f0f04
martioms01 <115492151+martioms01@users.noreply.github.com>
2024-03-15 17:45:15 +0100

    Update DynamoHashTagRepository.java

--- lab3-mastodon_noanswer/src/main/java/edu/upf/storage/DynamoHashTagRepository.java
+++ lab3-mastodon_noanswer/src/main/java/edu/upf/storage/DynamoHashTagRepository.java
@@ -4,7 +4,26 @@ import java.io.Serializable;
 import java.util.Collections;
 import java.util.List;
 
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.model.ResourceNotFoundException;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+
+
+
 import com.github.tukaaa.model.SimplifiedTweetWithHashtags;
+
+//import software.amazon.awssdk.services.dynamodb.model.DynamoDbException;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
+import java.util.Arrays;
+
 
 import edu.upf.model.HashTagCount;
 
@@ -16,11 +35,12 @@ public class DynamoHashTagRepository implements IHashtagRepository, Serializable
   final AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
     .withEndpointConfiguration(
   new AwsClientBuilder.EndpointConfiguration(endpoint, region)
-    ).withCredentials(new ProfileCredentialsProvider("upf"))
+    ).withCredentials(new ProfileCredentialsProvider("default"))
     .build();
 
   final DynamoDB dynamoDB = new DynamoDB(client);
-  final Table dynamoDBTable = dynamoDB.getTable(LsdsTwitterHashtags);
+  String TableName = "LsdsTwitterHashtags";
+  final Table dynamoDBTable = dynamoDB.getTable(TableName);
 
 
   @Override
@@ -29,12 +49,14 @@ public class DynamoHashTagRepository implements IHashtagRepository, Serializable
     
     for (String hashtag: hashtags) {
         
-      Item item = dynamoDBTable.getItem(new PrimaryKey("Hashtag", hashtag, "Language", language));
+      Item item = dynamoDBTable.getItem("hashtag", hashtag, "language", h.getLanguage());
 
-      if (item == null){ //pseudo
-        putItemInTable(dynamoDBTable, hashtag, h.getLanguage(), list(h.getTweetId()));
+      if (item == null && h.getLanguage() != null){
+        System.out.print(h.getLanguage());
+        putItemInTable(dynamoDBTable, hashtag, h.getLanguage(), Arrays.asList(h.getTweetId()));
       } else {
-        updateItemInTable(dynamoDBTable, hashtag, h.getLanguage(), h.getTweetId());
+        System.out.print("in");
+        updateItemInTable(dynamoDBTable, h.getTweetId(), item);
       }
     }
 
@@ -45,47 +67,42 @@ public class DynamoHashTagRepository implements IHashtagRepository, Serializable
     return Collections.emptyList(); // TODO IMPLEMENT ME
   }
 
-  public static void putItemInTable(Table dbt, String hashtag, String lan, List<Integer> tweet_id){
+  public static void putItemInTable(Table dbt, String hashtag, String lan, List<Long> tweet_id){
     
     try {
-      PutItemResponse response = table.putItem(new Item()
-              .withPrimaryKey("Hashtag", hashtag, "Language", language)
+      dbt.putItem(new Item()
+              .withPrimaryKey("hashtag", hashtag, "language", lan)
               .withInt("Counter", 1)
-              .withList("TweetIds", tweetIds));
+              .withList("TweetIds", tweet_id));
 
-      System.out.println(tableName + " was successfully updated. The request id is "
-              + response.sdkHttpResponse().xAmznRequestId());
+      System.out.println("Table was successfully updated.");
 
     } catch (ResourceNotFoundException e) {
-      System.err.format("Error: The Amazon DynamoDB table \"%s\" can't be found.\n", tableName);
+      System.err.format("Error: The Amazon DynamoDB table can't be found.\n");
       System.err.println("Be sure that it exists and that you've typed its name correctly!");
       System.exit(1);
-    } catch (DynamoDbException e) {
+    } catch (Exception e) {
       System.err.println(e.getMessage());
       System.exit(1);
     }
   }
 
-  public static void updateItemInTable(Table dbt, String hashtag, String lan,  Integer tweet_id){
+  public static void updateItemInTable(Table dbt, Long tweet_id, Item item){
     
-    Map<String, AttributeValue> itemkey = new HashMap<>();
-      itemkey.put("Hashtag", AttributeValue.builder().s(hashtag).build());
-      itemkey.put("Language", AttributeValue.builder().s(language).build());
-    
-    Map<String, AttributeValue> updatedAttributes = new HashMap<>();
-    //sumar 1 al count i afegir tweet id a la llista
-
     try {
-      UpdateItemRequest request = UpdateItemRequest.builder()
-        .tableName(dbt.tableName())
-        .key(itemkey)
-        .attributeUpdates(updatedAttributes)
-        .build();
 
-      dbt.updateItem(request);
+      Long cnt = item.getLong("Counter");
+      cnt++;
+      item.withLong("counter", cnt);
+      List<Long> tweetList = item.getList("TweetIds");
+      tweetList.add(tweet_id);
+      item.withList("TweetIds", tweetList);
+
+      dbt.putItem(item);
+
       System.out.println("The item in the DynamoDB table was successfully updated!");
 
-    } catch (DynamoDbException e) {
+    } catch (Exception e) {
         System.err.println("Unable to update item in DynamoDB table: " + e.getMessage());
         System.exit(1);
     }
