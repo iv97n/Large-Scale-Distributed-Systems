abd104f9904abe9983aa8162629168243be7071c
martioms01 <115492151+martioms01@users.noreply.github.com>
2024-03-15 17:45:50 +0100

    Update MastodonHashtags.java

--- lab3-mastodon_noanswer/src/main/java/edu/upf/MastodonHashtags.java
+++ lab3-mastodon_noanswer/src/main/java/edu/upf/MastodonHashtags.java
@@ -4,11 +4,21 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.streaming.Durations;
 import org.apache.spark.streaming.StreamingContext;
 import org.apache.spark.streaming.api.java.JavaDStream;
+import org.apache.spark.streaming.api.java.JavaPairDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
 
 import com.github.tukaaa.MastodonDStream;
 import com.github.tukaaa.config.AppConfig;
 import com.github.tukaaa.model.SimplifiedTweetWithHashtags;
+
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.streaming.api.java.JavaDStream;
+
+import java.util.Arrays;
+import java.util.List;
+
+import edu.upf.storage.DynamoHashTagRepository;
+import scala.Tuple2;
 
 public class MastodonHashtags {
 
@@ -19,9 +29,27 @@ public class MastodonHashtags {
                 JavaStreamingContext jsc = new JavaStreamingContext(sc);
                 jsc.checkpoint("/tmp/checkpoint");
 
-                JavaDStream<SimplifiedTweetWithHashtags> stream = new MastodonDStream(sc, appConfig).asJStream();
+                DynamoHashTagRepository repository = new DynamoHashTagRepository();
 
-                // TODO IMPLEMENT ME
+                JavaDStream<SimplifiedTweetWithHashtags> stream = new MastodonDStream(sc, appConfig).asJStream();
+                /*
+                long tweetId = 123456789L;
+                String text = "This is a sample tweet.";
+                long userId = 987654321L;
+                String userName = "example_user";
+                String language = "en";
+                long timestampMs = System.currentTimeMillis();
+                List<String> hashtags = Arrays.asList("example", "tweet");
+
+                SimplifiedTweetWithHashtags tweet = new SimplifiedTweetWithHashtags(
+                tweetId, text, userId, userName, language, timestampMs, hashtags);   
+                repository.write(tweet);   
+                */
+                stream.foreachRDD(rdd -> {
+                        rdd.foreach(tweet -> {
+                            repository.write(tweet);
+                        });
+                    });
 
                 // Start the application and wait for termination signal
                 jsc.start();
